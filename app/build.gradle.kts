de7fe60bc9ebac321acf1d5712e972ab30708609
Pa2 <pa2x2@email.com>
2026-07-18 07:44:44 +0200

    (chore): update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     defaultConfig {
         applicationId = "app.katari"
 
-        versionCode = 9
-        versionName = "1.3.0"
+        versionCode = 10
+        versionName = "1.3.1"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
