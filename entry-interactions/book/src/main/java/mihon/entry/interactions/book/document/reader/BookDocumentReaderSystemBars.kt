1d43889be40a5d5476183710a193f35d3499dc75
Pa2 <pa2x2@email.com>
2026-08-09 05:50:05 +0200

    (fix): align BOOK reader navigation bar color

--- entry-interactions/book/src/main/java/mihon/entry/interactions/book/document/reader/BookDocumentReaderSystemBars.kt
+++ entry-interactions/book/src/main/java/mihon/entry/interactions/book/document/reader/BookDocumentReaderSystemBars.kt
@@ -1,5 +1,6 @@
 package mihon.entry.interactions.book.document.reader
 
+import android.os.Build
 import android.view.Window
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,6 +13,9 @@ internal class BookDocumentReaderSystemBars(window: Window) {
     private val appUsesDarkStatusBarIcons = controller.isAppearanceLightStatusBars
 
     init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            window.isNavigationBarContrastEnforced = false
+        }
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
