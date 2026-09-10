e68067b220231f8b84db74a5576d1c977ffdef55
Kiryl Kvit <kiryl.kvit@gmail.com>
2026-09-10 11:10:04 +0200

    (build): update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
     defaultConfig {
         applicationId = "app.katari"
 
-        versionCode = 60
-        versionName = "1.8.4"
+        versionCode = 61
+        versionName = "1.8.5"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
